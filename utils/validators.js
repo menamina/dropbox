8c1c5e660d18a9123b34f7c407160be00d7bd009
Mena <dayjacs03@gmail.com>
2026-01-06 17:56:20 -0600

    validator

--- utils/validators.js
+++ utils/validators.js
@@ -9,6 +9,20 @@ const signUpValidator = [
     .isEmail()
     .withMessage("Enter a valid email"),
   body("username").trim().notEmpty().withMessage("username is required"),
-  body("password").notEmpty().isLength({ min: 8 })("Minimum 8 characters"),
+  body("password")
+    .notEmpty()
+    .withMessage("Password cannot be empty")
+    .isLength({ min: 8 })
+    .withMessage("Minimum 8 characters"),
   body("confirmPass").notEmpty().withMessage("Confirm password"),
+
+  function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("signup", {
+        errors: errors.array(),
+      });
+    }
+    next();
+  },
 ];
